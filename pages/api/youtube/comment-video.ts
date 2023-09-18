9d015feb0f56607f3d84c6559534983a3ebf590c
Alex-v-dev <yampi.ru@gmail.com>
2023-09-18 12:41:59 +0500

    Add prettier

--- pages/api/youtube/comment-video.ts
+++ pages/api/youtube/comment-video.ts
@@ -1,41 +1,44 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import {getAuthClient, getYoutube} from "@/server/utils/youtube";
-import {google, youtube_v3} from "googleapis";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { getYoutube } from "@/server/utils/youtube";
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse
+  res: NextApiResponse,
 ) {
   if (!req.body.videoId) {
-    throw new Error('body.videoId is required')
+    throw new Error("body.videoId is required");
   }
   if (!req.body.text) {
-    throw new Error('body.text is required')
+    throw new Error("body.text is required");
   }
 
   var youtube = getYoutube(req, res);
 
-  youtube.commentThreads.insert({
-    part: ['snippet'],
-    requestBody: {
-      snippet: {
-        videoId: req.body.videoId,
-        topLevelComment: {
-          snippet: {
-            videoId: req.body.videoId,
-            textOriginal: req.body.text
-          }
-        }
+  youtube.commentThreads.insert(
+    {
+      part: ["snippet"],
+      requestBody: {
+        snippet: {
+          videoId: req.body.videoId,
+          topLevelComment: {
+            snippet: {
+              videoId: req.body.videoId,
+              textOriginal: req.body.text,
+            },
+          },
+        },
+      },
+    },
+    // @ts-ignore
+    function (err, data) {
+      if (err) {
+        console.error("Error: " + err);
+        res.send(err);
       }
-    }
-  }, function (err, data) {
-    if (err) {
-      console.error('Error: ' + err);
-      res.send( err)
-    }
-    if (data) {
-      res.send( data)
-      console.log(data)
-    }
-  });
+      if (data) {
+        res.send(data);
+        console.log(data);
+      }
+    },
+  );
 }
